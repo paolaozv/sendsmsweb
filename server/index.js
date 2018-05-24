29bb39d7112f5b4faf21377d49ca106ef1cfcb29
Paola Ortiz de Zevallos <paola@juntoz.com>
2018-05-24 10:21:24 -0500

    add project send sms web

---
+++ server/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+const server = app.listen(4000);
+
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
